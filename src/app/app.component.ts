faefded482f866bb0e80d6e732e52f0e55c1e742
george francis <georgefrancis105@gmail.com>
2016-09-07 13:08:36 +1200

    Make list of dummy plugs and selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
 import '../../public/css/styles.css';
 
+export class Plug {
+  id: number;
+  description: string;
+}
+
+const PLUGS: Plug[] = [
+  { id: 1, description: 'Down Lights'},
+  { id: 2, description: 'Fan'},
+  { id: 3, description: 'Kitchen Speakers'}
+];
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -8,5 +19,11 @@ import '../../public/css/styles.css';
 })
 
 export class AppComponent {
-  message = 'Hello World'
+  message = 'Hello World';
+  plugs = PLUGS;
+  selectedPlug: Plug;
+
+  onSelect(plug: Plug): void {
+    this.selectedPlug = plug;
+  }
 }
