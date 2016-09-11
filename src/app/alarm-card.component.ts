af9b936e775bd417e10d71773e2ec311d46b6297
george francis <georgefrancis105@gmail.com>
2016-09-11 17:25:30 +1200

    Make alarm-card component

---
+++ src/app/alarm-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core'
+
+import { Alarm } from './alarm';
+
+@Component({
+  selector: 'alarm-card',
+  templateUrl: './alarm-card.component.html',
+  styleUrls: ['./alarm-card.component.css']
+})
+
+export class AlarmCardComponent {
+  @Input()
+  alarm: Alarm;
+  isOn = false;
+  toggleAlarm() {
+    this.alarm.on = !this.alarm.on;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { PlugsComponent } from './plugs.component';
 import { PlugCardComponent } from './plug-card.component';
 import { AlarmsComponent } from './alarms.component';
+import { AlarmCardComponent } from './alarm-card.component';
 
 import { PlugService } from './plug.service';
 import { AlarmService } from './alarm.service';
@@ -21,7 +22,8 @@ import { AlarmService } from './alarm.service';
     AppComponent,
     PlugsComponent,
     PlugCardComponent,
-    AlarmsComponent
+    AlarmsComponent,
+    AlarmCardComponent
   ],
   providers: [
     PlugService,
