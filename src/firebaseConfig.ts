20008159eb061b11d1e6df7c6f430f5ec41a06a5
george francis <georgefrancis105@gmail.com>
2016-09-16 14:07:07 +1200

    Make firebaseConfig file

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBuVD5OXCSQiHmhB_MENyY-en2YVrm9mbc",
+  authDomain: "angle-control.firebaseapp.com",
+  databaseURL: "https://angle-control.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "991417158966"
+};
