3d468a33b12883ab49ea2e5d102f0fa5bbed1c6e
george francis <georgefrancis105@gmail.com>
2016-09-17 15:06:31 +1200

    Refactor Firebase init in plugs.service

--- src/app/plug.service.ts
+++ src/app/plug.service.ts
@@ -8,20 +8,16 @@ import { Plug } from './plug';
 @Injectable()
 
 export class PlugService {
-  db: any;
   plugs: Plug[];
-  constructor() {
-    this.db = firebase;
-  }
   getPlugs(): Observable<Plug[]> {
     return Observable.create(observer => {
-      this.db.database().ref('plugs').on('value', snapshot => {
+      firebase.database().ref('plugs').on('value', snapshot => {
         let data = convertObjToArr(snapshot.val());
         data = data.map(plug => {
           plug.toggle = () => {
             let updates = {};
             updates['plugs/' + plug.id + '/on'] = !plug.on;
-            this.db.database().ref().update(updates);
+            firebase.database().ref().update(updates);
           }
           return plug;
         })
